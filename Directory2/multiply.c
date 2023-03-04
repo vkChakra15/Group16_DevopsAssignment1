608d34d8dbefaaa08838ee83d9f2d58ae45d28a0
Chakravarthi <vkchakravarthi15@gmail.com>
2023-03-04 13:02:27 +0530

    added multiply.c

---
+++ Directory2/multiply.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    int a,b;
+    int multiply;
+    printf("Enter  a and b:");
+    scanf("%d%d",&a,&b);
+    multiply=a*b;
+    printf("\n");
+    printf("The Multiplication of %d and %d is: %d",a,b,multiply);
+}
