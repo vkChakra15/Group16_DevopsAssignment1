35846c1fe3cc6b1de107aa3ec5525938fa47d239
Chakravarthi <vkchakravarthi15@gmail.com>
2023-03-04 12:29:21 +0530

    Multiplication added

--- Directory2/addition.c
+++ Directory2/addition.c
@@ -3,8 +3,13 @@
 int main(){
 
     int a,b;
+    int multiply;
     printf("Enter  a and b:");
     scanf("%d%d",&a,&b);
     int sum=a+b;
+    multiply=a*b;
+    
     printf("The sum of %d and %d is: %d",a,b,sum);
+    printf("\n");
+    printf("The Multiplication of %d and %d is: %d",a,b,multiply);
 }
