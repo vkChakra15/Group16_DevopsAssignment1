c2d6124584c664079a925c1d5985e5c84c3a9107
vkChakra15 <93901228+vkChakra15@users.noreply.github.com>
2023-03-04 12:02:47 +0530

    Update addition.c

--- Directory2/addition.c
+++ Directory2/addition.c
@@ -5,6 +5,6 @@ int main(){
     int a,b;
     printf("Enter  a and b:");
     scanf("%d%d",&a,&b);
-    int sum=a+b;
-    printf("The sum of %d and %d is: %d",a,b,sum);
+    int add=a+b;
+    printf("The sum of %d and %d is: %d",a,b,add);
 }
